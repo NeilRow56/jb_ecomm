2c143765aedfdcd2404d1103e9b3a292c0024f1e
NeilRow56 <neilrowland56@gmail.com>
2024-01-08 10:30:34 +0000

    Added DataTable with dummy data

--- src/app/(back-office)/dashboard/page.tsx
+++ src/app/(back-office)/dashboard/page.tsx
@@ -2,8 +2,20 @@ import Heading from '@/components/Heading'
 import LargeCards from '../_components/LargeCards'
 import SmallCards from '../_components/SmallCards'
 import DashboardCharts from '../_components/DashboardCharts'
+import { DataTable } from '@/components/DataTable'
+import { User, columns } from './_components/Columns'
 
-export default function Dashboard() {
+async function getUsers(): Promise<User[]> {
+  const res = await fetch(
+    'https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users'
+  )
+  const data = await res.json()
+
+  return data
+}
+
+export default async function Dashboard() {
+  const data = await getUsers()
   return (
     <div>
       <Heading title="Dashboard Overview" />
@@ -14,6 +26,13 @@ export default function Dashboard() {
       {/* Charts */}
       <DashboardCharts />
       {/* Table for Recent Orders */}
+
+      <section className="py-24">
+        <div className="container">
+          <h1 className="text-3xl font-bold">All Users</h1>
+          <DataTable columns={columns} data={data} />
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -67,29 +67,37 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex justify-between ">
-        <div className="flex items-center py-4 text-blue-800">
-          <Input
-            placeholder="Filter names..."
-            value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
-            onChange={(event) =>
-              table.getColumn('name')?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
-          />
+      <div className="flex w-full  items-center justify-between">
+        <div className="flex  gap-[196px]">
+          <div className="flex items-center py-4 text-blue-800">
+            <Input
+              placeholder="Filter names..."
+              value={
+                (table.getColumn('name')?.getFilterValue() as string) ?? ''
+              }
+              onChange={(event) =>
+                table.getColumn('name')?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+          </div>
+          <div className="flex items-center py-4 text-blue-800">
+            <Input
+              placeholder="Filter emails..."
+              value={
+                (table.getColumn('email')?.getFilterValue() as string) ?? ''
+              }
+              onChange={(event) =>
+                table.getColumn('email')?.setFilterValue(event.target.value)
+              }
+              className="mr-10 max-w-sm"
+            />
+          </div>
         </div>
-        <div className="flex items-center py-4 text-blue-800">
-          <Input
-            placeholder="Filter emails..."
-            value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
-            onChange={(event) =>
-              table.getColumn('email')?.setFilterValue(event.target.value)
-            }
-            className="mr-10 max-w-sm"
-          />
+        <div className="flex w-[400px] items-center justify-end">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="ml-auto">
+              <Button variant="outline" className="ml-auto text-blue-800">
                 Columns
               </Button>
             </DropdownMenuTrigger>
@@ -167,24 +175,57 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          className="text-blue-800"
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
+        <button
+          onClick={() => {
+            table.previousPage()
+          }}
           disabled={!table.getCanPreviousPage()}
+          className="border border-gray-300 p-1 px-2 disabled:opacity-30"
         >
-          Previous
-        </Button>
-        <Button
-          className="text-blue-800"
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
+          {'<'}
+        </button>
+        <button
+          onClick={() => {
+            table.nextPage()
+          }}
           disabled={!table.getCanNextPage()}
+          className="border border-gray-300 p-1 px-2 disabled:opacity-30"
         >
-          Next
-        </Button>
+          {'>'}
+        </button>
+
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="w-16 rounded border bg-transparent p-1"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value))
+          }}
+          className="bg-transparent p-2"
+        >
+          {[5, 10, 15, 20].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   )
