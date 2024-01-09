30d55984f855f2b69e6ec5167a74205dd6680845
NeilRow56 <neilrowland56@gmail.com>
2024-01-09 09:24:29 +0000

    Added dropdoen menu to notifications and profile

--- src/app/(back-office)/_components/Navbar.tsx
+++ src/app/(back-office)/_components/Navbar.tsx
@@ -1,26 +1,158 @@
-import { Bell, Menu, Sun, User } from 'lucide-react'
+import { Bell, Layout, LogOut, Menu, Settings, Sun, User } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <div className=" fixed top-0  flex h-16 w-full items-center justify-between  bg-slate-800 px-10 py-4 text-slate-50">
+    <div className=" fixed top-0  flex h-16 w-full items-center justify-between  overflow-hidden bg-slate-800 px-24  py-4 text-slate-50">
       <div className="flex">
         <button>
           <Menu />
         </button>
       </div>
-      <div className="flex space-x-3">
+      <div className="flex space-x-8">
         <button>
           <Sun />
         </button>
-        <button>
-          <Bell />
-        </button>
-        <button>
-          <User />
-        </button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <div className="flex items-center">
+              <Bell />
+
+              <span className="absolute right-[187px] top-[6px] flex h-5 w-5 items-center justify-center rounded-full bg-red-700 text-sm text-white">
+                5
+              </span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[350px] bg-slate-800 px-4 py-2 pr-4 text-slate-200">
+            <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <div className=" flex  items-center">
+                <div className="flex w-8">
+                  <Layout className=" h-4 w-4" />
+                </div>
+
+                <div className="flex flex-col">
+                  <span className="text-xs">Sam Sweet placed £300 order</span>
+                  <div className="flex flex-row">
+                    <Badge className="w-[85px] items-center bg-red-600 px-1  text-xs">
+                      <span className="flex w-full ">Out of Stock</span>
+                    </Badge>
+                    <h6 className=" pl-2 pt-[3px] text-xs text-slate-200">
+                      12 Dec 2023 - 12:40pm
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <div className=" flex w-full items-center">
+                <div className="flex w-8">
+                  <Image
+                    src="/profile.jpg"
+                    width={200}
+                    height={200}
+                    className="h-5 w-5  rounded-full"
+                    alt="profile picture"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-xs">Sam Sweet placed £300 order</span>
+                  <div className="flex flex-row">
+                    <Badge className="w-[85px] items-center bg-green-600 px-1 text-center  text-xs">
+                      <span className="flex w-full ">In Stock</span>
+                    </Badge>
+                    <h6 className="w-full pl-2 pt-[3px] text-xs text-slate-200">
+                      12 Dec 2023 - 12:40pm
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <div className=" flex  items-center">
+                <div className="flex w-8">
+                  <Layout className=" h-4 w-4" />
+                </div>
+
+                <div className="flex flex-col">
+                  <span className="text-xs">Sam Sweet placed £300 order</span>
+                  <div className="flex flex-row">
+                    <Badge className="w-[85px] items-center bg-red-600 px-1  text-xs">
+                      <span className="flex w-full ">Out of Stock</span>
+                    </Badge>
+                    <h6 className="pl-2 pt-[3px] text-xs text-slate-200">
+                      Dashboard
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <div className=" flex items-center">
+                <div className="flex w-8">
+                  <Layout className=" h-4 w-4" />
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-xs">Sam Sweet placed £300 order</span>
+                  <div className="flex flex-row">
+                    <Badge className="w-[85px] items-center bg-green-600 px-1 text-center  text-xs">
+                      <span className="flex w-full ">In Stock</span>
+                    </Badge>
+                    <h6 className="pl-2 pt-[3px] text-xs text-slate-200">
+                      Dashboard
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenuSeparator />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Image
+              src="/profile.jpg"
+              width={200}
+              height={200}
+              className="h-8 w-8 rounded-full"
+              alt="profile picture"
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="px-4 py-2 pr-4">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer ">
+              <Layout className="mr-2 h-4 w-4" />
+              <span>Dashboard</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Edit Profile</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Logout</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   )
